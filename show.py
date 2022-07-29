6d1513dc5ad6724cc2c660104d6a903e5a31f858
SepehrRasouli <sepehrrs06@gmail.com>
2022-07-29 13:56:56 +0430

    fixed bugs in show.py and changed stylings

--- show.py
+++ show.py
@@ -2,7 +2,6 @@ from rich import print
 from rich.panel import Panel
 from datetime import datetime,timedelta
 class Show:
-
     @staticmethod
     def _error_handler(func):
         def handler(*args,**kwargs):
@@ -13,23 +12,21 @@ class Show:
         return handler
 
     @_error_handler
-    @staticmethod
-    def calculate_end_time(entry):
+    def calculate_end_time(self,entry):
         start = datetime.strptime(entry[1],"%H:%M")
         end = start + timedelta(minutes=int(entry[2]))
         return end
 
     @_error_handler
-    @staticmethod
-    def show(data):
+    def show(self,data):
         data = sorted(data,key=lambda x:x[1])
-        for entry,index in enumerate(data):
-            end_time = calculate_end_time(entry)
+        for index,entry in enumerate(data):
+            end_time = self.calculate_end_time(entry)
             print(
                 Panel(
                     f"[bold green]{entry[0]}",
-                    title=f"[bold red]{entry[1]} | [bold cyan]Task Number:{index}",
-                    subtitle=f"[bold blue]{end_time.hour}:{end_time.minute} | {entry[2]}"
+                    title=f"[italic red]{entry[1]} | Task Number : {index}",
+                    subtitle=f"[italic blue]{end_time.hour}:{end_time.minute} | {entry[2]} Minutes"
                 ),"\n"
             )
 
