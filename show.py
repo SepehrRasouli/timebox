1b4ad9bd0f5d01832569c92d85f924ace17d2ad8
SepehrRasouli <sepehrrs06@gmail.com>
2022-07-29 13:35:54 +0430

    Finished show.py

---
+++ show.py
@@ -0,0 +1,35 @@
+from rich import print
+from rich.panel import Panel
+from datetime import datetime,timedelta
+class Show:
+
+    @staticmethod
+    def _error_handler(func):
+        def handler(*args,**kwargs):
+            try:
+                return func(*args,**kwargs)
+            except Exception as e:
+                raise e
+        return handler
+
+    @_error_handler
+    @staticmethod
+    def calculate_end_time(entry):
+        start = datetime.strptime(entry[1],"%H:%M")
+        end = start + timedelta(minutes=int(entry[2]))
+        return end
+
+    @_error_handler
+    @staticmethod
+    def show(data):
+        data = sorted(data,key=lambda x:x[1])
+        for entry in data:
+            end_time = calculate_end_time(entry)
+            print(
+                Panel(
+                    f"[bold green]{entry[0]}",
+                    title=f"[bold red]{entry[1]}",
+                    subtitle=f"[bold blue]{end_time.hour}:{end_time.minute} | {entry[2]}"
+                ),"\n"
+            )
+
